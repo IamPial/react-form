eff1cb33a0b1ac0ce6edb44f2340393a75a00f68
IamPial <pialname394@gmail.com>
2025-10-30 15:22:35 +0600

    initializing handleChange & handleSubmit

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,32 @@
 import InputGroup from "../components/shared/forms/InputGroup";
 import Button from "../components/ui/buttons/Button";
+import { useState } from "react";
+
+const init = {
+  title: "",
+  bio: "",
+  skills: "",
+};
 
 // After salat & lunch , then work with functionalities
 const App = () => {
+  const [values, setValues] = useState({ ...init });
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+
   return (
     <div className="root">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div
           style={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}
         >
@@ -13,16 +34,22 @@ const App = () => {
             name={"title"}
             label={"Title:"}
             placeholder={"Software Engineer"}
+            value={values.title}
+            onChange={handleChange}
           />
           <InputGroup
             name={"bio"}
             label={"Bio:"}
             placeholder={"Software Engineer"}
+            value={values.bio}
+            onChange={handleChange}
           />
           <InputGroup
             name={"skills"}
             label={"Skills:"}
             placeholder={"Software Engineer"}
+            value={values.skills}
+            onChange={handleChange}
           />
           <Button
             bg={"primary"}
